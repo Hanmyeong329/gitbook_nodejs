cbdf0edd7e708c33ca18cdd5823db4c480fa88f5
Hanmyeong329 <mytokki0323@gmail.com>
2021-09-30 18:13:08 +0900

    4.클라이언트 요청 GET까지 210930

---
+++ server_request_get.js
@@ -0,0 +1,29 @@
+var http = require('http');
+
+var url = require('url');
+
+var querystring = require('querystring');
+
+var server = http.createServer(
+    function(request,response){
+        console.log('----log start---');
+        var parsedUrl = url.parse(request.url);
+        console.log(parsedUrl);
+        var parsedQuery = querystring.parse(parsedUrl.query,'&','=');
+        console.log(parsedQuery);
+        console.log('---log end---');
+        response.writeHead(200, {'Content-Type':'text/html;charset=UTF-8'});
+        response.end('var1의 값은 '+parsedQuery.var1);
+      }
+
+);
+
+server.listen(8080, 
+    function(){
+    console.log('Server is running...');
+}
+);
+
+
+//node server_request_get.js
+//http://localhost:8080/?var1=newData&var2=153&var3=testdata2017 로 브라우저 이동
